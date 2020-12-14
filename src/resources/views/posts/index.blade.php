576b77137159a429e81db377c86fe4039b524d5c
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-12-14 22:53:51 +0900

    Laravel Mixのインストール

---
+++ src/resources/views/posts/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
+  <title>ゲームソフト投稿サイト</title>
+</head>
+<body>
+  <h1 class="text">Sassの勉強です</h1>
+  <p>Hello world</p>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/sass', function () {
+  return view('sass');
 });
+
+Route::get('/', 'postscontroller@index')->name('top');
