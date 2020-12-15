5f4097035f994d5b88d0556fe0123c8f5cd84fd3
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-12-15 22:03:41 +0900

    reset.scss,header.scssのファイルを作成、indexファイルにheader、nav、aタグ追加

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -19366,6 +19366,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/_reset.scss":
+/*!************************************!*\
+  !*** ./resources/sass/_reset.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -19378,14 +19389,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/_reset.scss ***!
+  \******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/hijikatahironori/new_gamepost/src/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/hijikatahironori/new_gamepost/src/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/hijikatahironori/new_gamepost/src/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/hijikatahironori/new_gamepost/src/resources/sass/_reset.scss */"./resources/sass/_reset.scss");
 
 
 /***/ })
--- src/resources/views/posts/index.blade.php
+++ src/resources/views/posts/index.blade.php
@@ -3,11 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
+  <link rel="stylesheet" href="{{ asset('/css/_reset.css')}}">
+  <!-- <link rel="stylesheet" href="{{ asset('/css/header.css')}}"> -->
   <title>ゲームソフト投稿サイト</title>
 </head>
 <body>
-  <h1 class="text">Sassの勉強です</h1>
-  <p>Hello world</p>
+  <header>
+    <nav class="header_title">
+      <a href="{{url('')}}">ゲームソフト投稿サイト</a>
+    <h1 class="text">Sassの勉強です</h1>
+    <p>Hello world</p>
+    </div>
+  </header>
 </body>
 </html>
