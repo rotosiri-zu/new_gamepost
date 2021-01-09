c77f9cd3323eeb7273bcfb9c99fb7b2b7f0f7c9c
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-01-09 21:23:17 +0900

    webpack.mix.jsにversionを追加、layoutsのasset記載をmixに追加

--- src/resources/views/layouts.blade.php
+++ src/resources/views/layouts.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="{{ asset('/css/_reset.css')}}">
-  <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
+  <link rel="stylesheet" href="{{ mix('/css/app.css')}}">
   <title>ゲームソフト投稿サイト</title>
 </head>
 <body>
