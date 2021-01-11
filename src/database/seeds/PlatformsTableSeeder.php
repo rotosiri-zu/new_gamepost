8319ea7181e7647835e65f40d7fb3a8b7a12d5f7
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-01-11 23:34:06 +0900

    PlatformsTableSeederにcsvのコード追加

--- src/database/seeds/PlatformsTableSeeder.php
+++ src/database/seeds/PlatformsTableSeeder.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
+use Goodby\CSV\Import\Standard\Lexer;
+use Goodby\CSV\Import\Standard\Interpreter;
+use Goodby\CSV\Import\Standard\LexerConfig;
+
 class PlatformsTableSeeder extends Seeder
 {
+
+    const CSV_FILENAME = '/../database/seeds/platform.csv'; 
+    
     /**
      * Run the database seeds.
      *
@@ -11,6 +18,26 @@ class PlatformsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->command->info('[Start] import data.');
+
+        $config = new LexerConfig();
+        // セパレーター指定、"\t"を指定すればtsvファイルとかも取り込めます
+        $config->setDelimiter(",");
+        // 1行目をスキップ
+        // $config->setIgnoreHeaderLine(true);
+        $lexer = new Lexer($config);
+        $interpreter = new Interpreter();
+        $interpreter->addObserver(function(array $row) {
+            // 登録処理
+            $genre = \App\Platform::create([
+                'id' => $row[0] ,
+                'platform_name' => $row[1] ,
+                'parent_id' => $row[2] 
+                ]);
+        });
+
+        $lexer->parse(app_path() . self::CSV_FILENAME, $interpreter);
+
+        $this->command->info('[End] import data.');
     }
 }
