90379c67d24c73e62b23e66dcf2db04f4c3b902e
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-01-20 23:21:11 +0900

    子要素のセレクトボックスの追加,scriptタグの追加

--- src/resources/js/components/PlatformComponent.vue
+++ src/resources/js/components/PlatformComponent.vue
@@ -3,7 +3,15 @@
     <label for="paltform">プラットフォーム</label>
     <select id="paltform_id" name="paltform_id" class="form-control" v-model="selected">
       <option disabled value="">選択してください</option>
-      <option value="" v-for=""></option>
+      <option value="parent_id" v-for="parent in parents"></option>
     </select>
   </div>
+  <div class="form-group">
+    <select class="form-control">
+    <option disabled value="">選択してください</option>
+    <option value="child_id" v-for="child in childs"></option>
+    </select>
 </template>
+
+<script>
+</script>
